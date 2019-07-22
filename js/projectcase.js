e39aa2e068ff2823f4ae6155c4cb4d87d7f83119
ainibao1987 <1227556724@qq.com>
2019-07-23 00:31:43 +0800

    2

--- js/projectcase.js
+++ js/projectcase.js
@@ -1,10 +1,41 @@
-$(function () {
-    let box = $(".item-box");
-    let content = $(".item-wrap");
-    let img = $(".item-img img");
-    let img2 = $(".item-img2 img")
+// $(function () {
+//     let box = $(".item-box");
+//     let content = $(".item-wrap");
+//     let img = $(".item-img img");
+//     let blank = $(".item-box .item-link");
+
+//     box.on("mouseenter", img, function (e) {
+//         // img.css({
+//         //     opacity: 1,
+//         //     transform: "scale(1.04)",
+//         // });
+//         blank.css({
+//             opacity: 0
+//         });
+//         // content.css({
+//         //     transform: "translateY(-66px)",
+//         //     color: "#fff",
+//         // })
+//     })
+//     box.on("mouseleave", img, function () {
+//         img.css({
+//             opacity: 0,
+//             transform: "scale(1)",
+//         });
+//         blank.css({
+//             opacity: 1
+//         })
+//         // content.css({
+//         //     transform: "translateY(0)",
+//         //     color: "#ccc",
+//         // })
+//     })
+//     $(document).scroll(function () {
+//         var pos = $(this).scrollTop();
+//         if (pos > somevalue) { //替换 somevalue 为具体数字
+//             //do something
+//         }
+//     });
 
 
-
-
-})
+// })
